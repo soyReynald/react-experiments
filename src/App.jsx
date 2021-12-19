6f911af8580390f0892ffcebc4b0d77aa60e627e
Reynald Ramirez <reynaldramirez@hotmail.com>
2021-12-19 18:50:14 -0400

    [I] Starting things...

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.png";
+import "./public/css/App.scss";
+
+function App() {
+    return (
+        <main>
+            <header>
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
+        </main>
+    );
+}
+
+export default App;
