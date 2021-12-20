d528bc1953ceb7c42ad82026c4fc1b26313135b8
Reynald Rodriguez <rrodriguez@aldereca.com>
2021-12-20 13:43:00 -0400

    [U] Going on with the React Router

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 
 function App() {
+    const url = "https://reactrouter.com/docs/en/v6/getting-started/tutorial";
     return (
         <>
             <Main>
                 <Header />
+                {`This docs are esential: `}<a href={url}>React Router</a>
+                
                 <Footer />
             </Main>
         </>
