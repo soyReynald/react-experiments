e57085125d49bd836953accd33e39e4dcf3cba5a
Reynald Rodriguez <rrodriguez@aldereca.com>
2021-12-23 10:40:43 -0400

    [P] A path to follow

---
+++ experiments/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Here I will create my own library of components</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
